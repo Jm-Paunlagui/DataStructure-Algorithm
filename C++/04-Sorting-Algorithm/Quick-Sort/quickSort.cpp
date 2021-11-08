50b7a23a72a0a523436ac2fef8aa5e65d50b1b95
JmPaunlagui <johnpaunlagui@gmail.com>
2021-11-08 18:38:16 +0800

    Individual

--- C++/04-Sorting-Algorithm/Quick-Sort/quickSort.cpp
+++ C++/04-Sorting-Algorithm/Quick-Sort/quickSort.cpp
@@ -1,123 +1,97 @@
-// QUICK SORT ALGORITHM
-/**
- * Quick sort is a highly efficient sorting algorithm and is based on partitioning of array of data into smaller arrays. 
- * A large array is partitioned into two arrays one of which holds values smaller than the specified value, say pivot, 
- * based on which the partition is made and another array holds values greater than the pivot value.
- * 
- *
- * Quicksort partitions an array and then calls itself recursively twice to sort the two resulting subarrays. 
- * This algorithm is quite efficient for large-sized data sets as its average and worst-case complexity are O(nLogn) and O(n2), respectively.
- * 
- * QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. 
- * There are many different versions of quickSort that pick pivot in different ways.
- * 
- * QUICK SORT PIVOT ALGORITHM Based on our understanding of partitioning in quick sort, 
- * we will now try to write an algorithm for it, which is as follows
- * 
- *  Step 1 - Choose the highest index value has pivot
- *  Step 2 - Take two variables to point left and right of the excluding pivot
- *  Step 3 - Left points to the low index
- *  Step 4 - Right points to the high
- *  Step 5 - While value at the left is less than the pivot, move right
- *  Step 6 - While value at right is greater than pivot move left
- *  Step 7 if both step 5 and step 6 does not match swap left and right
- *  Step 9 if left >= right, the point where they met is new pivot
- * 
- * Using pivot algorithm recursively, we end up with smaller possible partitions. Each partition is then processed for quick sort. 
- * We define recursive algorithm for quicksort as follows
- * 
- *  Step 1 - Make the right-most index value pivot
- *  Step 2 - Partition the array using pivot value
- *  Step 3 - quickSort left partition recursively
- *  Step 4 - quickSort right partition recursively
-*/
-
+/* C++ implementation of QuickSort */
 #include <bits/stdc++.h>
-
 using namespace std;
-// Function declaration
-void swap(int *num1, int *num2);
+
+void swap(int *a, int *b);
 int partition(int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
-void display(int arr[], int size);
+void printArray(int arr[], int size);
+void algoStep(int arr[], int size);
 
-int main(void)
+// Driver Code
+int main()
 {
-    int num;
-    cout << "Enter number of elements: ";
-    cin >> num;
-    int arrays[num];
-    cout << "Enter elements: " << endl;
-    for (int i = 0; i < num; i++)
-    {
-        cin >> arrays[i];
-    }
-    cout << "Arrays before sorting: ";
-    display(arrays, num);
-    quickSort(arrays, 0, num - 1);
-    cout << "Arrays after sorting: ";
-    display(arrays, num);
+    int arr[] = {10, 7, 8, 9, 1, 5};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Unsorted array: \n";
+    printArray(arr, n);
+    cout << "\n";
+    quickSort(arr, 0, n - 1);
+    cout << "\nSorted array: \n";
+    printArray(arr, n);
+    return 0;
 }
 
-// A function to swap two elements
-void swap(int *num1, int *num2)
+// A utility function to swap two elements
+void swap(int *a, int *b)
 {
-    int temp = *num1;
-    *num1 = *num2;
-    *num2 = temp;
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-/**
- *  This function takes last element as pivot, places the pivot element at its correct 
- *  position in sorted array, and places all smaller (smaller than pivot) to left of pivot
- *  and all greater elements to right of pivot
- */
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high];
-    int index = (low - 1);
-
+    int pivot = arr[high]; // pivot
+    cout << "pivot: " << pivot << endl;
+    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+    algoStep(arr, 6);
     for (int j = low; j <= high - 1; j++)
     {
-        // if the current element is smaller than the pivot
+
+        // If current element is smaller than the pivot
         if (arr[j] < pivot)
         {
-            index++; // increment index of smaller element
-            swap(&arr[index], &arr[j]);
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[index + 1], &arr[high]);
-    return (index + 1);
+    swap(&arr[i + 1], &arr[high]);
+    algoStep(arr, 6);
+    return (i + 1);
 }
 
-/**
- *  arr[] --> array to be sorted
- * low -->  Starting index
- * high --> Ending index
- */
-
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /**
-         * pi is partitioning index, arr[p] is now at the right
-         * place
-        */
+        /* pi is partitioning index, arr[p] is now
+		at right place */
         int pi = partition(arr, low, high);
 
-        /**
-        * Separately sort elements before
-        * partition and after partition 
-        */
+        // Separately sort elements before
+        // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
-
-void display(int arr[], int size)
+void algoStep(int arr[], int size)
 {
-    for (int index = 0; index < size; index++)
-        cout << arr[index] << " ";
+
+    for (int j = 0; j < size; j++)
+    {
+        cout << "[" << arr[j] << "]"
+             << "\t";
+    }
     cout << endl;
 }
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// This code is contributed by rathbhupendra
